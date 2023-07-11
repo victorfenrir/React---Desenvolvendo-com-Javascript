a756730ef4900117b4a02d2ccc16d703405dcc27
Victor Barra <victorbarrapereira@gmail.com>
2023-07-11 12:38:44 -0300

    React - Desenvolvendo com Javascript - aula 1

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner Principal da Página do Organo" />
+        </header>
+    )
+}
+

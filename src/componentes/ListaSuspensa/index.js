60f8bda3c0790a25c20cbe146870a4c893e03f15
Victor Barra <victorbarrapereira@gmail.com>
2023-07-21 11:05:35 -0300

    React - Desenvolvendo com Javascript - aula 3

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,10 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens)
-
     return(
         <div className='lista-suspensa'>
-            <label className='label'>{props.label}</label>
-            <select required={props.obrigatorio}>
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
                 {props.itens.map(item => 
                     <option key={item}>{item}</option>)}
             </select>
@@ -15,3 +13,4 @@ const ListaSuspensa = (props) => {
 }
 
 export default ListaSuspensa
+

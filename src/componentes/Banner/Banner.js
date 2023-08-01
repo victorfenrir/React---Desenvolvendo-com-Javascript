04f79e67e2d0d97b0fdafd088f580a2bc2599438
Victor Barra <victorbarrapereira@gmail.com>
2023-08-01 17:55:22 -0300

    React - Desenvolvendo com Javascript - aula final

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,14 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 export const Banner = () => {
     //JSX
     return (
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="Banner Principal da Página do Organo" />
-        </header>
+        <Fragment>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="Banner Principal da Página do Organo" />
+            </header>
+        </Fragment>
     )
 }
 
